b6c462cfce28b5009cc709d5b2b8612e9e9f19a8
IYU <issacyu2013@gmail.com>
2018-11-07 22:52:44 -0500

    Add a dummy post request to the purchaseContinueHandler for testing.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -82,7 +83,23 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        const order = {
+            customer:{
+                ingredients: this.state.ingredients,
+                price: this.state.totalPrice,
+                name: 'John Doe',
+                address: {
+                    street: 'ABCDE 1',
+                    zipCode: '12345',
+                    country: 'USA'
+                },
+                email: 'burger@burger.com',
+                deliveryMethod: 'Fastest'
+            }
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render() {
