5ad8768a18c73240b067509cfb76abfeb8d872a5
IYU <issacyu2013@gmail.com>
2018-12-15 21:35:07 -0500

    Refactor the burger builder project.
    1. Use redux thunk to fetch and save data asynchronously via axios.
    
    2. Refactor the action and reducer files.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -5,6 +5,8 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from '../../../store/actions/index';
 
 class ContactData extends Component {
     state = {
@@ -13,16 +15,11 @@ class ContactData extends Component {
         address: {
             street: '',
             postalCode: ''
-        },
-
-        loading: false
+        }
     }
 
     orderHandler = (event) => {
-        event.preventDefault();
-
-        this.setState({loading: true});
-        
+        event.preventDefault();        
         const order = {
             ingredients: this.props.ings,
             price: this.props.price,
@@ -37,14 +34,8 @@ class ContactData extends Component {
             },
             deliveryMethod: 'Fastest'
         }
-        axios.post('/orders.json', order)
-            .then( response => {
-                this.setState({loading: false});
-                this.props.history.push('/');
-            })
-            .catch( error => {
-                this.setState({loading: false})
-            });
+
+        this.props.onOrderBurger(order);
     }
 
     render () {
@@ -57,7 +48,7 @@ class ContactData extends Component {
                 <Button className={classes.Input} btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
-        if(this.state.loading) {
+        if(this.props.loading) {
             form = <Spinner />;
         }
         return (
@@ -71,9 +62,16 @@ class ContactData extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients,
-        price: state.totalPrice
+        ings: state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice,
+        loading: state.order.loading
     }
 };
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = dispatch => {
+    return {
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
